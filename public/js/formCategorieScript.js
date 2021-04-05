3969775c306881681b02f2ef2ecf764e63a0a4ef
ilham <ilham.lazar.01@gmail.com>
2021-04-05 22:17:40 +0200

    before last commit

--- public/js/formCategorieScript.js
+++ public/js/formCategorieScript.js
@@ -4,7 +4,6 @@ function fct1() {
   if (checkBox.checked == true){
      $("#thema1").css({"display": "block"});
      $("#matiere1").css({"display": "none"});
-      //document.getElementById('ici').innerHTML ='<label>Thematique</label><input type="text" name="Thematique"/>';
   } 
 }
 function fct2() {
@@ -12,21 +11,10 @@ function fct2() {
   if (checkBox.checked == true){
     $("#matiere1").css({"display": "block"});
     $("#thema1").css({"display": "none"});
-    //document.getElementById('ici').innerHTML ='<label>Matiere</label><input type="text" name="Matiere"/>';
+
   } 
 }
-    
-    /*$(document).ready(function() {
-      $("select.Type").change(function() {
-        var selectedCountry = $(".Type option:selected").text();
-        if (selectedCountry == "Vrai_Faux") {
-          alert("You have selected the language - Hindi");
-        } else if (selectedCountry == "Nepal") {
-          alert("You have selected the language - Nepali");
-        }
-      });
-    });
-    */
+
 function Clk() {
     alert("ok");
 }
